dbdbdb1d678f349077bc780d783e2c7a80fb6a01
FerdeDevs <pferdelson@gmail.com>
2025-03-11 11:04:15 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,sets,tsx}", "./components/**/*.{js,jsx,sets,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#0047D1',
+        blueDark: '#001b4f',
+        green: '#0A9D0E',
+      },
+    },
+  },
+  plugins: [],
+}
